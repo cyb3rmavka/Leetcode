3ef7ac2f2c2cc85a21fdd30b99d5f37f3021f97e
Mavka <134809168+cyb3rmavka@users.noreply.github.com>
2023-05-28 15:45:34 +0300

    Update taskN123.cpp

--- Hard/ Cpp/taskN123.cpp
+++ Hard/ Cpp/taskN123.cpp
@@ -9,24 +9,27 @@ Note: You may not engage in multiple transactions simultaneously (i.e., you must
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-	    const int N = prices.size();
-	    int *lMaxYield = new int[n]();
-      int *rMaxYield = new int[n]();
-	    int minCost = prices[0];
+		const int N = prices.size();
+		int *lMaxYield = new int[n]();
+    	int *rMaxYield = new int[n]();
+		int minCost = prices[0];
 	    
-      for (int i = 1; i < n; i++) {
-		    minCost = min(minCost, prices[i]);
-		    lMaxYield[i] = max(lMaxYield[i - 1], prices[i] - minCost);
+   		for (int i = 1; i < n; i++) {
+			minCost = min(minCost, prices[i]);
+			lMaxYield[i] = max(lMaxYield[i - 1], prices[i] - minCost);
 	    }
+		
 	    int maxCost = prices[n - 1];
 	    for (int i = n - 2; i >= 0 ; i--) {
 		    maxCost = std::max(maxCost, prices[i]);
 		    rMaxYield[i] = std::max(rMaxYield[i + 1], maxCost - prices[i]);
 	    }
+		
 	    int yield = 0;
 	    for (int i = 0; i < n; i++) {
 		    yield = std::max(yield, rMaxYield[i] + lMaxYield[i]);
 	    }
+		
 	    delete []lMaxYield;
 	    delete []rMaxYield;
 	    return yield;
