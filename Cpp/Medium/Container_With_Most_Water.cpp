b50563cffb3b9f5f50b7f162b14eb1430031ea59
Mavka <pavlenko.maksym.a@gmail.com>
2023-06-03 04:49:50 +0300

    Update Container_With_Most_Water.cpp

--- Cpp/Medium/Container_With_Most_Water.cpp
+++ Cpp/Medium/Container_With_Most_Water.cpp
@@ -10,30 +10,30 @@ Notice that you may not slant the container.
 
 class Solution {
 public:
-int maxArea(std::vector<int>& height) {
-    int max_square = 0;
-    int left = 0;
-    int right = height.size() - 1;
+    int maxArea(std::vector<int> &height) {
+        int max_square = 0;
+        int left = 0;
+        int right = height.size() - 1;
 
-    while (left < right) {
-        int h = std::min(height[left], height[right]);
-        int w = right - left;
-        int square = h * w;
-        max_square = std::max(max_square, square);
+        while (left < right) {
+            int h = std::min(height[left], height[right]);
+            int w = right - left;
+            int square = h * w;
+            max_square = std::max(max_square, square);
 
-        if (height[left] < height[right]) {
-            int curr = height[left];
-            while (left < right && height[left] <= curr) {
-                left++;
-            }
-        } else {
-            int curr = height[right];
-            while (left < right && height[right] <= curr) {
-                right--;
+            if (height[left] < height[right]) {
+                int curr = height[left];
+                while (left < right && height[left] <= curr) {
+                    left++;
+                }
+            } else {
+                int curr = height[right];
+                while (left < right && height[right] <= curr) {
+                    right--;
+                }
             }
         }
-    }
 
-    return max_square;
-}
+        return max_square;
+    }
 };
